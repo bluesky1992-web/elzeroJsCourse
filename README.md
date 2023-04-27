b25aea5189cf1f8a41b2c6da9e14ad98a6fa0cea
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-04-27 15:11:55 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Elzero javascript course Exercises
+
+this repo contain all exercises i have solved in this course 
---
+++ app.js
@@ -0,0 +1,17 @@
+// Lessons 1 to 9 
+
+// 1
+
+let h1 = document.createElement('h1')
+h1.innerHTML = 'Elzero'
+h1.style.color='blue'
+h1.style.fontSize='80px'
+h1.style.fontWeight='bold'
+h1.style.fontFamily='Arial'
+document.body.appendChild(h1)
+
+/////////////////
+
+
+// 2
+
